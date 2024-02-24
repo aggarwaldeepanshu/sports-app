d09ff2c706934a4c5711d5084e8b5515044e5ff0
Deepanshu Aggarwal <aggarwaldeepanshu96@gmail.com>
2024-02-24 13:46:53 +0530

    add changes to return match id, startTime and format

--- src/controllers/sport.js
+++ src/controllers/sport.js
@@ -3,15 +3,15 @@ const Sport = require('../models/sport');
 const getAllSportsToursAndMatches = async () => {
     const matches = await Sport.getAllSportsToursAndMatches();
     const res = {};
-    matches.forEach(match => {
-        const { sportName, tourName, matchName } = match;
+    matches.forEach(matchDetail => {
+        const { sportName, tourName, matchName, matchId, matchFormat, matchStartTime } = matchDetail;
         if (!res[sportName]) {
             res[sportName] = {};
         }
         if (!res[sportName][tourName]) {
             res[sportName][tourName] = [];
         }
-        res[sportName][tourName].push(matchName);
+        res[sportName][tourName].push({matchName, matchId, matchFormat, matchStartTime});
     });
     return res;
 }
