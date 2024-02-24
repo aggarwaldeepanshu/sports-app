8660a48dc35df3a05146c3ba53d2a209c11eb812
Deepanshu Aggarwal <aggarwaldeepanshu96@gmail.com>
2024-02-24 19:53:16 +0530

    add news support for matches, tours and sports

--- src/controllers/sport.js
+++ src/controllers/sport.js
@@ -3,19 +3,31 @@ const Sport = require('../models/sport');
 const getAllSportsToursAndMatches = async () => {
     const matches = await Sport.getAllSportsToursAndMatches();
     const res = {};
-    matches.forEach(matchDetail => {
-        const { sportName, tourName, matchName, matchId, matchFormat, matchStartTime } = matchDetail;
+    matches.forEach(match => {
+        const { sportName, tourName, matchName, matchId, matchStartTime, matchFormat } = match;
         if (!res[sportName]) {
             res[sportName] = {};
         }
         if (!res[sportName][tourName]) {
             res[sportName][tourName] = [];
         }
-        res[sportName][tourName].push({matchName, matchId, matchFormat, matchStartTime});
+        // adding match details as an object
+        let matchDetails = {
+            matchId, matchName, matchFormat, matchStartTime
+        }
+        res[sportName][tourName].push(matchDetails);
     });
     return res;
 }
 
+const findCorrespondingSportByTourId = async tourId => {
+    if(!tourId) {
+        throw new Error("Missing required parameter: tourId")
+    }
+    return await Sport.findCorrespondingSportByTourId(tourId)
+}
+
 module.exports = {
-    getAllSportsToursAndMatches: getAllSportsToursAndMatches
+    getAllSportsToursAndMatches: getAllSportsToursAndMatches,
+    findCorrespondingSportByTourId: findCorrespondingSportByTourId
 }
--- src/models/sport.js
+++ src/models/sport.js
@@ -1,13 +1,20 @@
 const mysql = require('../lib/mysql');
 
 const getAllSportsToursAndMatches = async () => {
-    const statement = 'select s.name as sportName, t.name as tourName, m.name as matchName, m.id as matchId, m.format as matchFormat, m.startTime as matchStartTime ' +
-        'from matches m inner join tours t on m.tourId = t.id ' +
-        'inner join sports s on t.sportId = s.id';
+    const statement = 'select s.name as sportName, t.name as tourName, m.name as matchName, m.id as matchId, m.startTime as matchStartTime, m.format as matchFormat ' +
+        'from matches m left join tours t on m.tourId = t.id ' +
+        'left join sports s on t.sportId = s.id';
     const parameters = [];
     return await mysql.query(statement, parameters);
 }
 
+const findCorrespondingSportByTourId = async tourId => {
+    const statement = 'select sportId from tours where tours.id = ?';
+    const parameter = [tourId];
+    return await mysql.query(statement, parameter);
+}
+
 module.exports = {
-    getAllSportsToursAndMatches: getAllSportsToursAndMatches
+    getAllSportsToursAndMatches: getAllSportsToursAndMatches,
+    findCorrespondingSportByTourId: findCorrespondingSportByTourId
 }
